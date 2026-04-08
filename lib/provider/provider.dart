9f40e60f76cb59f96ab01423dca21876b7f2894b
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-09 05:32:17 +0700

    menambah implementasi metode build pada kelas MyApp

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -13,5 +13,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FormPage(),
+    );
+  }
 }
