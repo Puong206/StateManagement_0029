f52889644417c3ccec098a4fe28f33128bcc6fcb
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-09 06:10:19 +0700

    menambah provider namaProvider dan emailProvider pada file riverpod.dart

--- lib/riverpod/riverpod.dart
+++ lib/riverpod/riverpod.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
+
+//provider
+final namaProvider = StateProvider<String>((ref) => "");
+final emailProvider = StateProvider<String>((ref) => "");
+
