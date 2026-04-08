ca78aee983d174c4e3b44dce9f1928cafd13afbd
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-09 06:12:26 +0700

    menambah implementasi FormPage dengan input nama dan email serta tombol submit

--- lib/riverpod/riverpod.dart
+++ lib/riverpod/riverpod.dart
@@ -6,3 +6,46 @@ import 'package:flutter_riverpod/legacy.dart';
 final namaProvider = StateProvider<String>((ref) => "");
 final emailProvider = StateProvider<String>((ref) => "");
 
+//UI
+class FormPage extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final nama = ref.watch(namaProvider);
+    final email = ref.watch(emailProvider);
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Form Riverpod")),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: "Nama"),
+              onChanged: (value) => 
+              ref.read(namaProvider.notifier).state = value,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: "Email"),
+              onChanged: (value) => 
+              ref.read(emailProvider.notifier).state = value,
+            ),
+            SizedBox(height: 20),
+            
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Submit sukses!')),
+                );
+              },
+              child: Text("Submit"),
+            ),
+            SizedBox(height: 10),
+
+            Text("Nama: $nama"),
+            Text("Email: $email"),
+          ],
+        ),
+      ),
+    );
+  }
+}
